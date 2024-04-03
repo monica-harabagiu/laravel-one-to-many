5155b5bc069a5a7cd679bb38aeea45d2cd116737
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-04-03 18:02:24 +0200

    model completed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Support\Str;
 
 class Project extends Model
@@ -18,7 +19,13 @@ class Project extends Model
         'slug'
     ];
 
-    public static function generateSlug($title){
+    public static function generateSlug($title)
+    {
         return Str::slug($title, '-');
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
     }
 }
